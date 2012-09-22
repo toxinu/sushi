1aacf88bfb0c1d81e1f7dc7bf51815e29ea39543
toxinu <toxinu@gmail.com>
2012-09-22 17:08:25 +0200

    0.2.7

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if sys.argv[-1] == 'publish':
 
 setup(
 	name='sushi',
-	version='0.2.6',
+	version='0.2.7',
 	description='Python package bundler',
 	long_description=	open('README.rst').read() + '\n\n' +
 						open('HISTORY.rst').read(),
