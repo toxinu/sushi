91e03f50b8d3970cb5c3233fc948a4f42df840eb
toxinu <toxinu@gmail.com>
2012-10-31 10:46:51 +0100

    Add get_version() method and 0.2.8 is out

--- setup.py
+++ setup.py
@@ -9,13 +9,23 @@ try:
 except ImportError:
 	from distutils.core import setup
 
+def get_version():
+    VERSIONFILE = 'sushi/__init__.py'
+    initfile_lines = open(VERSIONFILE, 'rt').readlines()
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    for line in initfile_lines:
+        mo = re.search(VSRE, line, re.M)
+        if mo:
+            return mo.group(1)
+    raise RuntimeError('Unable to find version string in %s.' % (VERSIONFILE,))
+
 if sys.argv[-1] == 'publish':
 	os.system('python setup.py sdist upload')
 	sys.exit()
 
 setup(
 	name='sushi',
-	version='0.2.7',
+	version=get_version(),
 	description='Python package bundler',
 	long_description=	open('README.rst').read() + '\n\n' +
 						open('HISTORY.rst').read(),
