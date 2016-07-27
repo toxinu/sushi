aa83e8198c114950e93df418007e20d3f55ad0e9
Geoffrey Lehée <glehee@oasiswork.fr>
2016-07-27 14:13:55 +0200

    0.3.6

--- sushi/__init__.py
+++ sushi/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'sushi'
-__version__ = '0.3.5'
+__version__ = '0.3.6'
 __author__ = 'Geoffrey Lehée'
 __license__ = 'AGPL3'
 __copyright__ = 'Copyright 2013 Geoffrey Lehée'
